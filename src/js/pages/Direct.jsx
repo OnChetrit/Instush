6eb18a9ddc31eb6ed7f57b808e78083ed8033216
On Chetrit <on7chetrit@gmail.com>
2021-11-17 21:17:55 +0200

    active pages

---
+++ src/js/pages/Direct.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+
+export const Direct = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    const { pathname } = history.location;
+    dispatch({ type: 'SET_PATHNAME', pathname });
+  }, []);
+
+  return <div className="direct">direct</div>;
+};
